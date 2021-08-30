1507d8faabbb7713569097ba070c1b1132c127bc
Richard Firdaus <richfir@gmail.com>
2021-08-30 14:43:45 +0800

    Tidy config files

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '^/api': {
-            target: 'http://localhost:8000',
+            target: 'http://localhost:8088',
             // target: 'http://192.168.100.150:8080',
             changeOrigin: true,
             secure: false,
--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,10 @@
 module.exports = {
     DB_HOST: 'mongodb://localhost:27017',
     // DB_HOST: 'mongodb://jzate:jzhipster@192.168.100.150:27017/?authSource=admin',
-    // DB_HOST: 'mongodb://jzate:jzhipster@172.105.225.25:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false',
     DB_NAME: 'ate_mongodb',
     DB_TEST_COLLECTION: 'ate_tests',
     DB_PROJECT_COLLECTION: 'ate_projects',
     HOST: 'http://localhost:8080', // client URL
-    // HOST: 'http://192.168.100.150:80',
-    // HOST: 'http://172.105.225.25:80',
     TOKEN_SECRET: 'jzhpstrcrp0222225678',
     MINUTES_LOGIN_TIME: 3,
     NORMAL_LOGIN_DAYS: 7,
